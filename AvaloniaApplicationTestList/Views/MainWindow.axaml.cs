03c0f80dcfb01b8b75dc74337e1def0f3bbd18f9
492494 <eric.morera-ventallo@alstomgroup.com>
2024-03-25 09:55:03 +0100

    remove bug fix tabcontrol

--- AvaloniaApplicationTestList/Views/MainWindow.axaml.cs
+++ AvaloniaApplicationTestList/Views/MainWindow.axaml.cs
@@ -17,7 +17,7 @@ public partial class MainWindow : Window
         //this.Main.testList.BorderListBox.Height = this.Height - 150;
         this.Main.testList.TraceListBox.Height = this.Height - 150;
         this.Main.testList.TraceListBox.Width = this.Width;
-        this.Main.TabControlMain.Height = this.Height - 50;
+        //this.Main.TabControlMain.Height = this.Height - 50;
     }
 
 
